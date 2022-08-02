14c75f4ad4abe09f52275b3631cc1914b0b5d8d7
yarramsrilatha <110217646+yarramsrilatha@users.noreply.github.com>
2022-08-02 16:41:46 +0530

    Delete examportalbackend directory

--- examportalbackend/src/main/java/com/main/Repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.Repository;
-
-import com.main.model.exam.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
